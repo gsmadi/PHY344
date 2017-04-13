7e0b5a324e355d64f8561a59695bce33a175f262
Gabriel Smadi <gabriel.smadi@gmail.com>
2017-04-12 23:30:24 -0400

    Mag grav plot

--- lab4/mag_grav_torque.py
+++ lab4/mag_grav_torque.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-m = 1.3E-3
+m = 1.3E-3  # [kg]
+delta_m = 0.1E-3  # [kg]
 g = 9.8  # [m*s^2]
 
+current = np.array([2.5, 2.7, 2.9, 3.1, 3.3, 3.4, 3.7, 3.8, 4.0])
 b_field = np.array([0.00375, 0.00405, 0.00435, 0.00465, 0.00495,
                     0.00510, 0.00555, 0.00570, 0.0060])
-
+delta_B = b_field*np.sqrt(2.28E-8 + (1.70E-10)*current**2)
+delta_B
 r = np.array([6.7, 7.8, 8.7, 10.2, 10.6, 11.4, 12.3, 12.8, 14.0])
+delta_r = 0.3*np.ones(len(r))  # [cm]
 
 coefficients, residuals = np.polyfit(b_field, r, deg=1, cov=True)
+
+y_fit = coefficients[0]*b_field + coefficients[1]
 
 slope = coefficients[0]*1E-2
 intercept = coefficients[1]*1E-2
@@ -18,8 +24,25 @@ slope_uncertainty = np.sqrt(residuals[0][0])*1E-2
 intercept_uncertainty = np.sqrt(residuals[1][1])*1E-2
 
 mu = m*g*slope
-delta_mu = 0.1
+delta_mu = mu * np.sqrt((delta_m/m)**2 + (slope_uncertainty/slope)**2)
 
 print "Slope: {} +/- {}".format(slope, slope_uncertainty)
 print "Intercept: {} +/- {}".format(intercept, intercept_uncertainty)
-print "\n\nmu = {} +/- {}".format(mu, )
+print "\n\nmu = {} +/- {}".format(mu, delta_mu)
+
+# Plot data points and quadratic fit
+fig, ax = plt.subplots()
+
+ax.plot(b_field, y_fit, label="Fit", linestyle="--")
+
+ax.errorbar(b_field, r, xerr=[delta_B, delta_B],
+            yerr=[delta_r, delta_r], fmt='o', label="Observed")
+
+ax.set(title="Magnetic torque equals Gravitational torque",
+       xlabel="Magnetic field stength (T)",
+       ylabel="Radius (cm)")
+ax.legend(loc='upper left')
+plt.grid(True)
+fig.set_dpi(800)
+fig.savefig('plots/mag_grav.png')
+fig.show()
